a74e156348631adf2810197751b8217bbaa2c920
chuangshu <1397153057@qq.com>
2017-04-29 10:49:43 +0800

    goforword

---
+++ Application/Common/Conf/db.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: rehellinen
+ * Date: 2017/4/14
+ * Time: 17:56
+ */
+return array(
+    'DB_TYPE' => 'mysql',
+    'DB_HOST' => '127.0.0.1',
+    'DB_USER' => 'root',
+    'DB_PWD' => '123456',
+    'DB_PORT' => 3306,
+    'DB_NAME' => 'tgw',
+    'DB_CHARSET' => 'utf8',
+);
---
+++ Application/Runtime/Cache/Home/2a94b8bda04ff446dcedd42276fc99ee.php
@@ -0,0 +1,457 @@
+<?php if (!defined('THINK_PATH')) exit();?> 	
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+	<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Goforword</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="Free HTML5 Template by FREEHTML5" />
+	<meta name="keywords" content="free html5, free template, free bootstrap, html5, css3, mobile first, responsive" />
+	
+
+  
+
+  <!-- Facebook and Twitter integration -->
+	<meta property="og:title" content=""/>
+	<meta property="og:image" content=""/>
+	<meta property="og:url" content=""/>
+	<meta property="og:site_name" content=""/>
+	<meta property="og:description" content=""/>
+	<meta name="twitter:title" content="" />
+	<meta name="twitter:image" content="" />
+	<meta name="twitter:url" content="" />
+	<meta name="twitter:card" content="" />
+
+	<!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+	<link rel="shortcut icon" href="Public/images/favicon.png">
+	
+	<link href='https://fonts.googleapis.com/css?family=Work+Sans:400,300,700|Montserrat:400,700' rel='stylesheet' type='text/css'>
+	
+	<!-- Animate.css -->
+	<link rel="stylesheet" href="Public/css/animate.css">
+	<!-- Icomoon Icon Fonts-->
+	<link rel="stylesheet" href="Public/css/icomoon.css">
+	<!-- Simple Line Icons -->
+	<link rel="stylesheet" href="Public/css/simple-line-icons.css">
+	<!-- Owl Carousel -->
+	<link rel="stylesheet" href="Public/css/owl.carousel.min.css">
+	<link rel="stylesheet" href="Public/css/owl.theme.default.min.css">
+	<!-- Bootstrap  -->
+	<link rel="stylesheet" href="Public/css/bootstrap.css">
+
+	
+	<link rel="stylesheet" href="Public/css/style.css">
+
+
+	<!-- Modernizr JS -->
+	<script src="Public/js/modernizr-2.6.2.min.js"></script>
+	<!-- FOR IE9 below -->
+	<!--[if lt IE 9]>
+	<script src="js/respond.min.js"></script>
+	<![endif]-->
+	<style type="text/css">
+	.center{
+		width: auto;
+  		display: table;
+  		margin-left: auto;
+  		margin-right: auto;
+	}
+	</style>
+	</head>
+	<body>
+	<header role="banner" id="fh5co-header">
+			<div class="container">
+				<!-- <div class="row"> -->
+			    <nav class="navbar navbar-default">
+		        <div class="navbar-header">
+		        	<!-- Mobile Toggle Menu Button -->
+					<a href="#" class="js-fh5co-nav-toggle fh5co-nav-toggle" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar"><i></i></a>
+		          	<a class="navbar-brand" href="index.html">Goforword<span>.</span></a> 
+		        </div>
+		        <div id="navbar" class="navbar-collapse collapse">
+		          <ul class="nav navbar-nav navbar-right">
+		            <li class="active"><a class="white" href="#"data-nav-section="home"><span>首页</span></a></li>
+		            <li><a href="#" data-nav-section="tour"><span>服务介绍</span></a></li>
+		            <li><a href="#" data-nav-section="features"><span>贴心服务</span></a></li>
+		            <li><a href="#" data-nav-section="testimonials"><span>下单指南</span></a></li>
+		            <li><a href="#" data-nav-section="pricing"><span>价格</span></a></li>
+		          </ul>
+		        </div>
+			    </nav>
+		  </div>
+	</header>
+
+	<div id="slider" data-section="home">
+		<div class="owl-carousel owl-carousel-main owl-carousel-fullwidth">
+			<!-- You may change the background color here. -->
+		    <div class="item" style="background: #00868B;">
+		    	<div class="container" style="position: relative;">
+		    		<!--change animate photos here-->
+		    		<div class="row animate-box" data-animate-effect="fadeIn">
+					    <div class="col-md-7 col-sm-7">
+			    			<div class="fh5co-owl-text-wrap">
+						    	<div class="fh5co-owl-text">
+						    		<h1 class="fh5co-lead ">App for IOS</h1>						
+						    	</div>
+						    </div>
+					    </div>
+					    <div class="col-md-4 col-md-push-1 col-sm-4 col-sm-push-1 mobile-image">
+					    	<div class="mobile"><img src="Public/images/ios.png" alt="Free HTML5 Template by FREEHTML5"></div>
+					    </div>
+		    		</div>
+		    		<!--change animate photos here-->
+		    	</div>
+		    </div>
+			<!-- You may change the background color here.  -->
+		    <div class="item" style="background: #00868B;">
+		    	<div class="container" style="position: relative;">
+		    		<div class="row animate-box" data-animate-effect="fadeIn">
+		    			<div class="col-md-7 col-md-push-1 col-md-push-5 col-sm-7 col-sm-push-1 col-sm-push-5">
+			    			<div class="fh5co-owl-text-wrap">
+						    	<div class="fh5co-owl-text">
+						    		<h1 class="fh5co-lead ">App for Andriod</h1>
+						    	</div>
+						    </div>
+					    </div>
+					    
+					    <div class="col-md-4 col-md-pull-7 col-sm-4 col-sm-pull-7 mobile-image">
+					    	<div class="mobile"><img src="Public/images/andriod.png" alt="Free HTML5 Template by FREEHTML5"></div>
+					    </div>
+
+		    		</div>
+		    	</div>
+		    </div>
+
+		   
+		</div>
+	</div>
+    
+
+	<div id="fh5co-tour" data-section="tour">
+		<div class="container">
+			<div class="row row-bottom-padded-lg">
+				<div class="col-md-10 col-md-offset-1">
+					<div class="col-md-4 animate-box" data-animate-effect="fadeInLeft">
+						<img src="Public/images/screen-1-1-login.jpg" alt="Free HTML5 Bootstrap Template" class="img-responsive img-feature-1">	
+					</div>
+					<div class="col-md-7 col-md-push-1">
+						<h2>高水平、高质量的专家库</h2>
+						<p>包含各高校及学界资深学者等，为平台的质量提供了厚实的保障。赵友斌（暨南大学翻译学院教授兼院长）、陈毅平（暨南大学翻译学院教授兼副院长），林巍教授（南昆士兰大学(University of Southern Queensland)博士），均在语用学、翻译学造诣颇深。胡彦、刘小丽，暨南大学信息科技学院资深导师。殷炼乾（暨南大学国际商学院副教授），曾任国家开发银行，湖北分行，信贷经理、中级经济师，指导融资和风险规避。</p>
+						<p><a href="#" class="btn btn-primary">Learn more</a></p>	
+					</div>
+					
+				</div>
+			</div>
+
+			<div class="row row-bottom-padded-lg">
+				<div class="col-md-10 col-md-offset-1">
+					<div class="col-md-4 col-md-push-8 animate-box" data-animate-effect="fadeInRight">
+						<img src="Public/images/screen-1-8-list.jpg" alt="Free HTML5 Bootstrap Template" class="img-responsive img-feature-1">	
+					</div>
+					<div class="col-md-7 col-md-pull-4">
+						<h2>专业，负责的译员</h2>
+						<p>皆雅的译员，不仅要拥有出色的语言能力，还要拥有强大的专业背景。客户提出意见，译者免费对译文进行修改。如客户在多次修改后仍不满意，皆雅力求让客户绝对放心，不满意，不付钱。</p>
+						<p><a href="#" class="btn btn-primary">Learn more</a></p>	
+					</div>
+					
+				</div>
+			</div>
+
+			<div class="row">
+				<div class="col-md-10 col-md-offset-1">
+					<div class="col-md-4 animate-box" data-animate-effect="fadeInLeft">
+						<img src="Public/images/screen-3-7-groups.jpg" alt="Free HTML5 Bootstrap Template" class="img-responsive img-feature-1">	
+					</div>
+					<div class="col-md-7 col-md-push-1">
+						<h2>严格，完善的保障机制</h2>
+						<p>首先，建立自有高质量译者平台。平台主要由项目组成员及外聘全职雇员组成，如果出现纠纷无法得到解决，则平台将承接这一翻译任务。为防止客户恶意投诉其他译者，以便按原价寻求自有平台翻译，每次自有平台进行翻译，都会收取额外的一笔手续费。其次，设置以自有平台译员为核心的“圆心模式”，每个自有平台的译员，平时都会和部分已注册的活跃译员建立紧密联系，给出翻译建议的同时，对后者工作进行监督。最后，设立惩罚机制，如无法联系上译者，或译者译文明显存在敷衍了事痕迹，则采取封禁的措施。</p>
+						<p><a href="#" class="btn btn-primary">Learn more</a></p>	
+					</div>
+					
+				</div>
+			</div>
+
+		</div>
+	</div>
+	
+	<div id="fh5co-features" data-section="features">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12 section-heading text-center">
+					<h2 class="animate-box" data-animate-effect="fadeIn">贴心服务</h2>
+					<div class="row animate-box" data-animate-effect="fadeIn">
+						<div class="col-md-8 col-md-offset-2">
+							<h3>皆雅坚持为顾客想更多，就算这些细节被忽略</h3>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="row row-bottom-padded-sm">
+				<div class="col-md-6 col-sm-6 col-xs-6 col-xxs-12 fh5co-service animate-box" data-animate-effect="fadeIn">
+					<div class="fh5co-icon"><i class="icon-present"></i></div>
+					<div class="fh5co-desc">
+						<h3>便捷服务</h3>
+						<p>皆雅全年无休，保证订单快速响应，实现秒出报价和工期，项目进度可跟踪，确保译文及时交付，并且可以与译员实时沟通。</p>
+					</div>	
+				</div>
+				<div class="col-md-6 col-sm-6 col-xs-6 col-xxs-12 fh5co-service animate-box" data-animate-effect="fadeIn">
+					<div class="fh5co-icon"><i class="icon-eye"></i></div>
+					<div class="fh5co-desc">
+						<h3>高性价比</h3>
+						<p>缩短翻译供应链，将您的每一分钱都用于译稿本身和保证翻译质量。我们承诺您在皆雅的翻译成本比同质量传统翻译成本低 30% 左右。 </p>
+					</div>
+				</div>
+				<div class="clearfix visible-sm-block visible-xs-block"></div>
+				<div class="col-md-6 col-sm-6 col-xs-6 col-xxs-12 fh5co-service animate-box" data-animate-effect="fadeIn">
+					<div class="fh5co-icon"><i class="icon-speedometer"></i></div>
+					<div class="fh5co-desc">
+						<h3>质量保障</h3>
+						<p>皆雅入驻译员均具有强大的行业专业背景、出色的翻译能力和极高的敬业度，同时由内部质量专家团队负责质量仲裁。皆雅承诺不满意，不付钱。 </p>
+					</div>	
+				</div>
+				<div class="clearfix visible-sm-block visible-xs-block"></div>
+				<div class="col-md-6 col-sm-6 col-xs-6 col-xxs-12 fh5co-service animate-box" data-animate-effect="fadeIn">
+					<div class="fh5co-icon"><i class="icon-heart"></i></div>
+					<div class="fh5co-desc">
+						<h3>交易担保</h3>
+						<p>皆雅提供支付担保，您付款后翻译费用由皆译稿满意后，正式付款给译员，并可提供发票。皆雅全程监控，交易无忧。 </p>
+					</div>
+				</div>
+				<div class="clearfix visible-sm-block visible-xs-block"></div>
+			</div>
+		</div>
+	</div>
+
+	<div id="fh5co-testimonials" data-section="testimonials">		
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12 section-heading text-center">
+					<h2 class="animate-box" data-animate-effect="fadeIn">下单指南</h2>
+					<div class="row animate-box" data-animate-effect="fadeIn">
+						<div class="col-md-8 col-md-offset-2 subtext">
+							<h3>在皆雅，仅需三步即可完成整个交易流程，其余的工作统统由皆雅帮您搞定：</h3>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-4">
+					<div class="box-testimony animate-box" data-animate-effect="fadeIn">
+						<blockquote>
+							<span class="quote"><span><i class="icon-quote-left"></i></span></span>
+							<img src="Public/images/upload.jpg" alt="Free HTML5 Bootstrap Template" class="img-responsive img-feature-1">
+							<p class="text-center">&ldquo;上传文件&ldquo;</p>
+						</blockquote>
+					</div>
+					
+				</div>
+				<div class="col-md-4">
+					<div class="box-testimony animate-box" data-animate-effect="fadeIn">
+						<blockquote>
+							<span class="quote"><span><i class="icon-quote-left"></i></span></span>
+							<img src="Public/images/money.jpg" alt="Free HTML5 Bootstrap Template" class="img-responsive img-feature-1">
+							<p class="text-center">&ldquo;付款下单&rdquo;</p>
+						</blockquote>
+					</div>
+					
+					
+				</div>
+				<div class="col-md-4">
+					<div class="box-testimony animate-box" data-animate-effect="fadeIn">
+						<blockquote>
+							<span class="quote"><span><i class="icon-quote-left"></i></span></span>
+							<img src="Public/images/download.jpg" alt="Free HTML5 Bootstrap Template" class="img-responsive img-feature-1"a>
+							<p class="text-center">&ldquo;下载译文&rdquo;</p>
+						</blockquote>
+					</div>
+					
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div id="fh5co-pricing" data-section="pricing">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12 section-heading text-center">
+					<h2 class="animate-box" data-animate-effect="fadeIn">Price</h2>
+					<div class="row animate-box" data-animate-effect="fadeIn">
+						<div class="col-md-8 col-md-offset-2 subtext">
+							<h3>您每次下单，皆雅都会同时提供三种质量级别对应的价格。您可以结合自己的实际需求选择适合您的翻译质量级别和对应报价，让每一分钱都花在刀刃上，打造极致性价比。皆雅缩短了翻译供应链，去除中间商赚差价，让您用最少的钱换来最合适的译稿。皆雅承诺，比同质量传统翻译供应商节省 30% 成本。</h3>
+						</div>
+				</div>
+			</div>
+		</div>
+
+		<!--选择翻译语言与目标语言-->
+		<div style="margin-bottom: 2em;" class="row">
+            <form method="post" id="content">
+                <div class="col-md-4 col-sm-4">
+                    <div style="display:inline" class="dropdown animate-box">
+                        <select class="target2" name="target2">
+                            <option value="english" selected="selected" name="english">english</option>
+                            <option value="chinese" name="english">chinese</option>
+                            <option value="japanese" name="japanese">japanese</option>
+                        </select>
+                    </div>
+                </div>
+
+                <div class="col-md-4 col-sm-4">
+                    <div style="display:inline" class="dropdown animate-box">
+                        <select class="target3" name="target3">
+                            <option value="chinese" selected="selected">chinese</option>
+                            <option value="english">english</option>
+                            <option value="闽南话">闽南话</option>
+                        </select>
+                    </div>
+                </div>
+                <button class="btn btn-lg btn-primary btn-block animate-box" id ="check" type="button" onclick="login.check()" >查询</button>
+            </form>
+        </div>
+	
+		<div class="row">
+				<div class="col-md-4 col-sm-6">
+					<div class="price-box animate-box" data-animate-effect="fadeIn">
+						<h2 class="pricing-plan">实用级</h2>
+						<div class="price"><sup class="currency">$</sup>7<small>/mo</small></div>
+						<hr>
+						<p>
+							如果您在下单时选择实用级，您得到的译文表达流畅，语义通顺，无低级错误，基本能够体现原文所描述的内容。适用于一般性资料的翻译，满足阅读、参考等使用目的。实用级订单由至少具有 1-3 年翻译经验的译员处理。
+						</p>
+						<a href="#" class="btn btn-default btn-sm">Get started</a>
+					</div>
+				</div>
+				<div class="col-md-4 col-sm-6">
+					<div class="price-box animate-box" data-animate-effect="fadeIn">
+						<h2 class="pricing-plan">标准级</h2>
+						<div class="price"><sup class="currency">$</sup>19<small>/mo</small></div>
+						<p>Basic customer support for small business</p>
+						<hr>
+						<p>
+							如果您在下单时选择标准级，您得到的译文语义正确无误，另外还会根据行业领域、文体类型等加以润色。适用于偏专业性资料的翻译，如邀请函、用户指南、技术手册、宣传页等。标准级订单由至少具有 3-7 年相关行业翻译经验的译员处理，通过翻译、校审两个流程，尽力确保翻译质量。
+						</p>
+						<a href="#" class="btn btn-default btn-sm">Get started</a>
+					</div>
+				</div>
+				<div class="clearfix visible-sm-block"></div>
+				<div class="col-md-4 col-sm-6 animate-box" data-animate-effect="fadeIn">
+					<div class="price-box popular">
+						<h2 class="pricing-plan">出版级</h2>
+						<div class="price"><sup class="currency">$</sup>79<small>/mo</small></div>
+						<p>Basic customer support for small business</p>
+						<hr>
+						<p>
+							如果您在下单时选择出版级，您得到的译文语言优美，专业术语翻译精准，全部译文均经过资深译员或行业领域专家 (SME) 的校审。适用于网站、软件界面、标书、合同及定向行业领域内的专业资料翻译。出版级订单由具有 7 年以上相关行业翻译经验的译员处理。
+						</p>
+						<a href="#" class="btn btn-primary btn-sm">Get started</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	
+	<div class="fh5co-cta" style="background-image:url(images/hero.jpg)">
+		<div class="overlay"></div>
+		<div class="container">
+			<div class="col-md-8 col-md-offset-2 text-center">
+				<h3>联系方式：rabyxizi@foxmail.com</h3>
+				<p><a href="#" class="btn btn-primary btn-outline btn-lg">Get started</a></p>
+			</div>
+		</div>
+	</div>
+
+	<footer id="footer" role="contentinfo">
+		<div class="container">
+			<div class="row row-bottom-padded-sm">
+				<div class="col-md-12">
+					
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-12 text-center">
+					<ul class="social social-circle">
+						<li><a href="#"><i class="icon-twitter"></i></a></li>
+						<li><a href="#"><i class="icon-facebook"></i></a></li>
+						<li><a href="#"><i class="icon-youtube"></i></a></li>
+						<li><a href="#"><i class="icon-pinterest"></i></a></li>
+						<li><a href="#"><i class="icon-linkedin"></i></a></li>
+						<li><a href="#"><i class="icon-instagram"></i></a></li>
+						<li><a href="#"><i class="icon-dribbble"></i></a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</footer>
+	
+	
+	
+	<!-- jQuery -->
+	<script src="Public/js/jquery.min.js"></script>
+	<!-- jQuery Easing -->
+	<script src="Public/js/jquery.easing.1.3.js"></script>
+	<!-- Bootstrap -->
+	<script src="Public/js/bootstrap.min.js"></script>
+	<!-- Waypoints -->
+	<script src="Public/js/jquery.waypoints.min.js"></script>
+	<!-- Owl Carousel -->
+	<script src="Public/js/owl.carousel.min.js"></script>
+	<!-- Main JS (Do not remove) -->
+	<script src="Public/js/main.js"></script>
+
+
+    <script type="text/javascript">
+       /* $(".target2").change(function(e) {
+            url = "http://test.php";
+            data = {0:e.target.value}
+            alert(data[0])
+            $.post('get.php',data, function(result) {
+                alert(result)
+            },"JSON");
+        });
+
+        $(".target3").change(function(e) {
+            url = "http://test.php";
+            data = {1:e.target.value}
+            alert(data[1])
+            $.post('get.php',data, function(result) {
+                alert(result)
+            },"JSON");
+        });*/
+/*
+        $("#check").click(function(e) {
+            url = "http://get.php";
+            native =document.getElementsByClassName("target2").value;
+            goal = document.getElementsByClassName("target3").value;
+            data = {0:goal,1:native}
+            alert(data[0])
+            $.post('get.php',data, function(result) {
+                alert(result)
+            },"JSON");
+        })
+        */
+
+       $("#check").click(function(){
+
+           var data = $("#content").serializeArray();
+           //console.log(data);
+           postData = { };
+           $(data).each(function(i){
+               postData[this.name] = this.value;
+           });
+
+            alert(postData['target2'])
+           alert(postData['target3'])
+           url = "get.php";
+
+           $.post(url,postData,function(result) {
+               alert("success");
+           },'JSON');
+       });
+    </script>
+	</body>
+</html>
